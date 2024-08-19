f797f727c689fa3698b56a4779a9368484163204
Ilky <ilkyandre98@gmail.com>
2024-08-19 17:38:42 -0300

    refactor: changing card post links

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -6,27 +6,26 @@ import Link from "next/link";
 
 export const CardPost = ({ post }) => {
   return (
-    <Link href={`/posts/${post.slug}`} className={styles.link}>
-      <article className={styles.card}>
-        <header className={styles.header}>
-          <figure>
-            <Image
-              width={438}
-              height={133}
-              src={post.cover}
-              alt={`Capa do post de titulo: ${post.title}`}
-            />
-          </figure>
-        </header>
-        <section className={styles.body}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </section>
-        <footer className={styles.footer}>
-          <Avatar name={post.author.username} imageSrc={post.author.avatar} />
-        </footer>
-      </article>
-    </Link>
+    <article className={styles.card}>
+      <header className={styles.header}>
+        <figure>
+          <Image
+            width={438}
+            height={133}
+            src={post.cover}
+            alt={`Capa do post de titulo: ${post.title}`}
+          />
+        </figure>
+      </header>
+      <section className={styles.body}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link href={`/posts/${post.slug}`}>Ver detalhes</Link>
+      </section>
+      <footer className={styles.footer}>
+        <Avatar name={post.author.username} imageSrc={post.author.avatar} />
+      </footer>
+    </article>
   );
 };
 
